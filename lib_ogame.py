62b56f874d028479cd4d84d1ea2acf6c094964fe
François Schmidts <francois.schmidts@gmail.com>
2013-07-29 15:06:56 +0200

    on améliore les mises à jours de la base de données interne

--- lib_ogame.py
+++ lib_ogame.py
@@ -10,6 +10,8 @@ class Ogame(selenium):
     def __init__(self, mother=None, planets=[]):
         selenium.__init__(self,
                 "localhost", 4444, "*chrome", "http://ogame.fr/")
+        self.current_position = None
+        self.current_page = None
         self.mother, self.planets = mother, planets
         self.start()
 
@@ -27,16 +29,46 @@ class Ogame(selenium):
         if not self.planets:
             self.get_planets()
 
+    def __split_text(self, xpath, split_on='\n'):
+        return self.get_text(xpath).split(split_on)
+
     def update_planet_resources(self, planet):
         planet['resources'] = {}
-        for res_type in ['metal_box', 'crystal_box',
-                'deuterium_box', 'energy_box']:
-            res = self.get_text("//li[@id='%s']" % res_type).split('.')
-            planet['resources'][res_type[:-4]] = int(''.join(res))
+        try:
+            for res_type in ['metal_box', 'crystal_box',
+                    'deuterium_box', 'energy_box']:
+                res = self.__split_text("//li[@id='%s']" % res_type, '.')
+                planet['resources'][res_type[:-4]] = int(''.join(res))
+        except Exception:
+            pass
+
+    def update_planet_buildings(self, planet):
+        planet['constructions'] = constructions = {}
+        try:
+            for ctype in ('building', 'storage'):
+                for const in self.__split_text("//ul[@id='%s']" % ctype):
+                    if not const.strip():
+                        continue
+                    constructions[ctype] = {}
+                    name, level = const.strip().rsplit(' ', 1)
+                    constructions[ctype][name] = int(''.join(level.split('.')))
+        except Exception:
+            pass
+
+    def update_planet_fleet(self, planet):
+        planet['fleet'] = {}
+        try:
+            for fleet_type in ('military', 'civil'):
+                planet['fleet'][fleet_type] = {}
+                for fleet in self.__split_text("//ul[@id='%s']" % fleet_type):
+                    name, quantity = fleet.strip().rsplit(' ', 1)
+                    planet['fleet'][fleet_type][name] = int(quantity)
+        except Exception:
+            pass
 
     def get_planets(self, full=False):
         self.planets, xpath = {}, "//div[@id='planetList']"
-        for position, planet in enumerate(self.get_text(xpath).split('\n')):
+        for position, planet in enumerate(self.__split_text(xpath)):
             name, coords = re.split('\[?\]?', planet.split('\n')[0])[:2]
             self.planets[position + 1] = planet = {'name': name.strip()}
             planet['coords'] = [int(coord) for coord in coords.split(':')]
@@ -44,24 +76,26 @@ class Ogame(selenium):
 
             if not full:
                 continue
-
             self.go_to(planet, 'Ressources')
-
-            for const_list in ('building', 'storage'):
-                xpath = "//ul[@id='%s']" % const_list
-                for const in self.get_text(xpath).split('\n'):
-                    if not const.strip():
-                        continue
-                    planet[const_list] = {}
-                    name, level = const.strip().rsplit(' ', 1)
-                    planet[const_list][name] = int(''.join(level.split('.')))
+            self.go_to(planet, 'Flotte')
 
     def go_to(self, planet, page):
-        self.click("//div[@id='planetList']/div[%d]/a" % (planet['position']))
-        self.wait_for_page_to_load(DEFAULT_WAIT_TIME)
+        if self.current_position != planet['position']:
+            self.click("//div[@id='planetList']/div[%d]/a"
+                    % (planet['position']))
+            self.current_position = planet['position']
+            self.wait_for_page_to_load(DEFAULT_WAIT_TIME)
+
+        if self.current_page != page:
+            self.click("link=%s" % page)
+            self.current_page = page
+            self.wait_for_page_to_load(DEFAULT_WAIT_TIME)
+
         self.update_planet_resources(planet)
-        self.click("link=%s" % page)
-        self.wait_for_page_to_load(DEFAULT_WAIT_TIME)
+        if page == 'Ressources':
+            self.update_planet_resources(planet)
+        elif page == 'Flotte':
+            self.update_planet_fleet(planet)
 
     def send_ressources(self, src, dst, content={}):
         #metal = content.get('metal', 'all')
@@ -85,6 +119,7 @@ class Ogame(selenium):
         self.click("css=#allresources > img")
         self.click("css=#start > span")
         self.wait_for_page_to_load(DEFAULT_WAIT_TIME)
+        self.update_planet_fleet(src)
 
     def rapatriate(self, dst=None):
         if not self.planets:
